fff28c994edcb2388a61013d86aa00b6ba60f070
balumanobiram <bmanobiram@gmail.com>
2025-08-10 20:33:18 +0530

    #login skelton with basic css

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     const res = await fetch("http://127.0.0.1:5000/login", {
@@ -11,26 +24,58 @@ export default function Login() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password })
     });
-    const data = await res.json();
-    alert(data.message);
+    if (res.status === 200) {
+      navigate("/dashboard");
+    } else {
+      const data = await res.json();
+      alert(data.message);
+    }
+    
   };
 
   return (
-    <form onSubmit={handleLogin} style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      /><br /><br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      /><br /><br />
-      <button type="submit">Login</button>
-    </form>
+
+    <div className="flex items-center justify-center min-h-screen"><Card className="w-full max-w-sm">
+          <CardHeader>
+              <CardTitle>Login to your account</CardTitle>
+              <CardDescription>
+                  Enter your username below to login to your account
+              </CardDescription>
+              <CardAction>
+                  <Button variant="link">Sign Up</Button>
+              </CardAction>
+          </CardHeader>
+          <form onSubmit={handleLogin}>
+          <CardContent>
+              
+                  <div className="flex flex-col gap-6">
+                      <div className="grid gap-2">
+                          <Label htmlFor="text">Username</Label>
+                          <Input
+                              id="username"
+                              type="text"
+                              placeholder="Username"
+                              onChange={e => setUsername(e.target.value)}
+                              required />
+                      </div>
+                      <div className="grid gap-2">
+                          <div className="flex items-center">
+                              <Label htmlFor="password">Password</Label>
+                              
+                          </div>
+                          <Input id="password" type="password" onChange={e => setPassword(e.target.value)} required />
+                      </div>
+                  </div>
+                  <br></br>
+              <CardFooter className="flex-col gap-2">
+              <Button type="submit" className="w-full">
+                  Login
+              </Button>
+          </CardFooter>
+          </CardContent>
+          
+          </form>
+      </Card>
+      </div>
   );
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/dashboard";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/dashboard", element: <Dashboard />}
 ]);
